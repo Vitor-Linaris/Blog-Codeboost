614eeb22c70ec37a70797a95887491c18b0f4250
Vitor-Linaris <vitor.linaris@gmail.com>
2023-05-27 10:10:32 -0300

    feat: menu-mobile added

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -47,4 +47,5 @@ const btnMobile = document.getElementById("js-btn-mobile");
 
 btnMobile.addEventListener("click", () => {
   btnMobile.classList.toggle("is-active");
+  document.documentElement.classList.toggle("menu-opened");
 });
